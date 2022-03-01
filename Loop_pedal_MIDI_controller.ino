a714b6455e5555614ccbb5b9f6f8e5c1a2870150
qnguyen <qnguyen@student.42.fr>
2022-03-01 19:03:30 +0200

    one button at pin D3 to send note

--- Loop_pedal_MIDI_controller.ino
+++ Loop_pedal_MIDI_controller.ino
@@ -1,16 +1,29 @@
-#define button 2
-#define LED 8
+#include <MIDI.h>
+#define button 3
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(button, INPUT);
-  pinMode(LED, OUTPUT);
+  MIDI.begin(MIDI_CHANNEL_OFF);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int i;
+  int state;
 
+  state = 0;
   i = digitalRead(button);
-  digitalWrite(LED, i);
+  if (i == HIGH && state == 0)
+  {
+  	MIDI.sendNoteOn(102, 127, 1);
+    state = 1;
+  }
+  else if (i == LOW && state == 1)
+  {
+  	MIDI.sendNoteOff(102, 0, 1);
+    state = 0;
+  }
 }
